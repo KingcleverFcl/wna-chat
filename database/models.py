5149ce07e50154bcb7614d887800d4e16038268c
KingcleverFcl <freddyinghtmare2010@gmail.com>
2025-04-19 22:04:52 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,8 +1,16 @@
 import asyncpg
 from typing import Optional
 import os
+from .db import db
+from sqlalchemy import select
+from database.tables import users
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+async def get_user_language(user_id: int) -> str:
+    query = select(users.c.language).where(users.c.user_id == user_id)
+    result = await db.fetch_one(query)
+    return result["language"] if result and result["language"] else "en"
 
 async def connect_db():
     return await asyncpg.connect(DATABASE_URL)
